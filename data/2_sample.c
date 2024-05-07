4a8a43a8430177be1e29d854faeeadcb541ecbae
emoration <3388356471@qq.com>
2024-05-07 13:32:37 +0800

    update data

--- data/2_sample.c
+++ data/2_sample.c
@@ -7,10 +7,8 @@ int (int x, int y) {  // missing function name
 printf(char* str) {  // missing function type
     put_c(str);
 }
-
 float global_float = ; // missing value
 global_float = 3.14;  // unexpected statement
-
 int main() {
     int a=1, flag = true true;  // multiple value
     int arr[5][];  // incomplete array declaration
@@ -18,13 +16,10 @@ int main() {
     char char c = 'c';  // multiple type
     char str[20] = "Hello"  // missing semicolon
     a=1;
-
     int add(int x, int y) {  // unexpected nested function
         return x + y;
     }
-
     printf("%d\n", ); // missing argument
-
     if (flag) {
         if (c == 'c')
             ;
@@ -33,11 +28,9 @@ int main() {
         else  // unexpected else
             ;
     }
-
     for (int i = 0, float f = 0; i < 5; i+=1, ) {  // multiple declaration, incomplete expression
         printf("%d\n", i);
     }
-
     while (b > 0) {
         do
             b -= a[;  // missing closing bracket
@@ -46,11 +39,9 @@ int main() {
         b += 1;
         while(a);
     }
-
     {
         local_var = (1 + 2) * 3 + ( ;  // unmatched parenthesis
         int local_var;
     // missing closing brace
-
     return 0;
 }
